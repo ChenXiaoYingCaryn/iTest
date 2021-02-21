03c9485366ff923fcb423a585ff5104612f3940d
ChanV <1277077741@qq.com>
2021-02-21 11:56:42 +0800

    新建itest-eureka-7001模块

---
+++ itest-eureka-7001/src/main/java/com/itest/itesteureka7001/ItestEureka7001Application.java
@@ -0,0 +1,13 @@
+package com.itest.itesteureka7001;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItestEureka7001Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItestEureka7001Application.class, args);
+    }
+
+}
---
+++ itest-eureka-7001/src/test/java/com/itest/itesteureka7001/ItestEureka7001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itest.itesteureka7001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItestEureka7001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
