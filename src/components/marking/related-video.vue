cc1a7cc8a72e981238c95d95636c75b4edbb4aff
yuan <838003282@qq.com>
2021-02-22 23:24:07 +0800

    评分页面新增推荐模块

---
+++ src/components/marking/related-video.vue
@@ -0,0 +1,127 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="related-top">
+        <div class="container-title-large">相关视频</div>
+        <div class="more-btn">
+          <el-button plain type="mini">换一换<span class="el-icon-refresh"></span></el-button>
+          <el-button plain type="mini">更多<span class="el-icon-arrow-right"></span></el-button>
+        </div>
+      </div>
+      <div class="related-video-list">
+        <div class="related-video-item">
+          <div class="top-img">
+            <img src="./marking-img/beauty.jpg" alt="">
+            <span class="top-video-label top-video-audience el-icon-view">{{videoViews}}</span>
+            <span class="top-video-label top-video-time">{{videoTime}}</span>
+          </div>
+          <div class="video-content">
+            <a href="#" :title="videoTitle">{{videoTitle}}</a>
+            <a href="#">{{videoUpdater}}</a>
+            <div>{{videoUpdateTime}}</div>
+          </div>
+        </div>
+        <div class="related-video-item"></div>
+        <div class="related-video-item"></div>
+        <div class="related-video-item"></div>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'related-video',
+  data () {
+    return {
+      videoTime: '4:17',
+      videoTitle: '[MV]なんでもないや Cover (nandemonaiya) - radwimps Cover by yurisa [君の名はost]',
+      videoUpdater: 'yurisa',
+      videoUpdateTime: '2019.4.21',
+      videoViews: '10万'
+    }
+  }
+}
+</script>
+
+<style scoped>
+*{
+  box-sizing: border-box;
+}
+.container {
+    /* margin: 10px 10vw; */
+    /* margin: 0 10vw; */
+    background-color: bisque;
+}
+
+/* 标题及按钮 */
+.related-top {
+  position: relative;
+}
+
+.more-btn {
+  position: absolute;
+  top: 50%;right: 0;
+  transform: translateY(-50%);
+  display: flex;
+  background-color: green;
+}
+
+.el-button {
+  padding-left: 8px;
+  padding-right: 5px;
+}
+
+.container-title-large {
+    color: #EFCD6D;
+    font-size: 30px;
+    font-weight: bold;
+    margin-bottom: 15px;
+    margin-left: 15px;
+    line-height: 50px;
+}
+
+/* 视频推荐块布局 */
+.related-video-list {
+  display: flex;
+  justify-content: space-between;
+}
+
+.related-video-item {
+  width: 280px;
+  height: 260px;
+  background-color: pink;
+}
+
+/* 视频推荐图片和上面的参数 */
+.related-video-item .top-img {
+  position: relative;
+  height: 157.5px;
+}
+
+.related-video-item .top-img img {
+  /* width: auto;
+  height: auto; */
+  max-width: 100%;
+  max-height: 100%;
+}
+
+.related-video-item .top-img .top-video-label {
+  background-color: #EFCD6D;
+  position: absolute;
+}
+
+.related-video-item .top-img .top-video-time {
+  right: 0;bottom: 0;
+}
+
+.related-video-item .top-img .top-video-audience {
+  left: 0;bottom: 0;
+}
+.related-video-item .video-content a{
+  display: block;
+}
+
+</style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,20 +1,47 @@
-<template>
-  <div class="container">
-    <div class="content">
-      aaa
+ <template>
+  <div>
+    <div class="ctn">
+      <transition name="el-zoom-in-center">
+        <div class="box" v-show="show">el-zoom-in-center</div>
+      </transition>
+      <transition name="el-zoom-in-top">
+        <div class="box" v-show="show">el-zoom-in-top</div>
+      </transition>
+      <transition name="el-zoom-in-bottom">
+        <div class="box" v-show="show">el-zoom-in-bottom</div>
+      </transition>
     </div>
+    <el-button type="primary"  @click="zoom()">缩放</el-button>
   </div>
 </template>
-
 <script>
 export default {
-  name: 'test'
+  name: 'test',
+  data () {
+    return {
+      show: true
+    }
+  },
+  methods: {
+    zoom () {
+      this.show = !this.show
+    }
+  }
 }
 </script>
-
-<style scoped>
-.container {
-  background-color: yellow;
+ <style lang="css" scoped>
+ .ctn{
+   width: 1000px;
+   height: 300px;
+ }
+.box {
+  display: inline-block;
+  width: 200px;
+  height: 200px;
+  background-color: greenyellow;
+  text-align: center;
+  line-height: 200px;
+  color: #fff;
+  margin: 0 20px 20px 0;
 }
-
 </style>
