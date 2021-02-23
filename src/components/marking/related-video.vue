38fb80fb555e03e8a6c389dac9b3ec0c16e77f04
yuan <838003282@qq.com>
2021-02-24 01:29:26 +0800

    完善视频推荐模块

--- src/components/marking/related-video.vue
+++ src/components/marking/related-video.vue
@@ -16,8 +16,8 @@
             <span class="top-video-label top-video-time">{{videoTime}}</span>
           </div>
           <div class="video-content">
-            <a href="#" :title="videoTitle">{{videoTitle}}</a>
-            <a href="#">{{videoUpdater}}</a>
+            <a href="#" class="video-title" :title="videoTitle">{{videoTitle}}</a>
+            <a href="#" class="video-up" >{{videoUpdater}}</a>
             <div>{{videoUpdateTime}}</div>
           </div>
         </div>
@@ -39,8 +39,29 @@ export default {
       videoTime: '4:17',
       videoTitle: '[MV]なんでもないや Cover (nandemonaiya) - radwimps Cover by yurisa [君の名はost]',
       videoUpdater: 'yurisa',
-      videoUpdateTime: '2019.4.21',
+      videoUpdateTime: '2018/2/21 10:08:33',
       videoViews: '10万'
+    }
+  },
+  methods: {
+    function () {
+    }
+  },
+  created: function () {
+  },
+  mounted: {
+    calculateTimeDiff (){
+      var newTime = new Date()
+      var updateTime = new Date(this.videoUpdateTime)
+      var timeDiff = newTime.getTime() - updateTime.getTime()
+      var dayDiff = Math.floor(timeDiff / (24 * 3600 * 1000))
+      console.log(dayDiff + '天')
+      var weekDiff = dayDiff / 7
+      console.log(weekDiff + '周')
+      var monthDiff = dayDiff / 30
+      console.log(monthDiff + '月')
+      var yearDiff = monthDiff / 12
+      console.log(yearDiff + '年')
     }
   }
 }
@@ -91,14 +112,19 @@ export default {
 
 .related-video-item {
   width: 280px;
-  height: 260px;
+  /* height: 260px; */
   background-color: pink;
 }
 
 /* 视频推荐图片和上面的参数 */
 .related-video-item .top-img {
+  cursor: pointer;
   position: relative;
   height: 157.5px;
+}
+
+.related-video-item .top-img:hover {
+
 }
 
 .related-video-item .top-img img {
@@ -120,8 +146,26 @@ export default {
 .related-video-item .top-img .top-video-audience {
   left: 0;bottom: 0;
 }
+
 .related-video-item .video-content a{
   display: block;
+  margin: 0 auto;
+}
+
+.related-video-item .video-content .video-title {
+  height: 38px;
+  overflow: hidden;
+  margin: 10px 0 8px;
+
+  /* 解决文字溢出 */
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+}
+
+.related-video-item .video-content .video-up {
+  font-size: 14px;
+  color: #999;
 }
 
 </style>
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,47 +1,18 @@
  <template>
   <div>
-    <div class="ctn">
-      <transition name="el-zoom-in-center">
-        <div class="box" v-show="show">el-zoom-in-center</div>
-      </transition>
-      <transition name="el-zoom-in-top">
-        <div class="box" v-show="show">el-zoom-in-top</div>
-      </transition>
-      <transition name="el-zoom-in-bottom">
-        <div class="box" v-show="show">el-zoom-in-bottom</div>
-      </transition>
-    </div>
-    <el-button type="primary"  @click="zoom()">缩放</el-button>
+    <video-item></video-item>
   </div>
 </template>
 <script>
+import videoItem from '../components/video-item.vue'
 export default {
+  components: { videoItem },
   name: 'test',
   data () {
     return {
-      show: true
-    }
-  },
-  methods: {
-    zoom () {
-      this.show = !this.show
     }
   }
 }
 </script>
  <style lang="css" scoped>
- .ctn{
-   width: 1000px;
-   height: 300px;
- }
-.box {
-  display: inline-block;
-  width: 200px;
-  height: 200px;
-  background-color: greenyellow;
-  text-align: center;
-  line-height: 200px;
-  color: #fff;
-  margin: 0 20px 20px 0;
-}
 </style>
