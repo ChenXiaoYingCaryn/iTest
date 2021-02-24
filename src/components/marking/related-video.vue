100091888a3613c614a9de5ff02c9353f32eb4f1
yuan <838003282@qq.com>
2021-02-24 23:21:56 +0800

    视频推荐模块完成

--- src/components/marking/related-video.vue
+++ src/components/marking/related-video.vue
@@ -9,21 +9,10 @@
         </div>
       </div>
       <div class="related-video-list">
-        <div class="related-video-item">
-          <div class="top-img">
-            <img src="./marking-img/beauty.jpg" alt="">
-            <span class="top-video-label top-video-audience el-icon-view">{{videoViews}}</span>
-            <span class="top-video-label top-video-time">{{videoTime}}</span>
-          </div>
-          <div class="video-content">
-            <a href="#" class="video-title" :title="videoTitle">{{videoTitle}}</a>
-            <a href="#" class="video-up" >{{videoUpdater}}</a>
-            <div>{{videoUpdateTime}}</div>
-          </div>
-        </div>
-        <div class="related-video-item"></div>
-        <div class="related-video-item"></div>
-        <div class="related-video-item"></div>
+        <video-item></video-item>
+        <video-item></video-item>
+        <video-item></video-item>
+        <video-item></video-item>
       </div>
 
     </div>
@@ -32,15 +21,12 @@
 </template>
 
 <script>
+import videoItem from '../video-item.vue'
 export default {
+  components: { videoItem },
   name: 'related-video',
   data () {
     return {
-      videoTime: '4:17',
-      videoTitle: '[MV]なんでもないや Cover (nandemonaiya) - radwimps Cover by yurisa [君の名はost]',
-      videoUpdater: 'yurisa',
-      videoUpdateTime: '2018/2/21 10:08:33',
-      videoViews: '10万'
     }
   },
   methods: {
@@ -50,19 +36,6 @@ export default {
   created: function () {
   },
   mounted: {
-    calculateTimeDiff () {
-      var newTime = new Date()
-      var updateTime = new Date(this.videoUpdateTime)
-      var timeDiff = newTime.getTime() - updateTime.getTime()
-      var dayDiff = Math.floor(timeDiff / (24 * 3600 * 1000))
-      console.log(dayDiff + '天')
-      var weekDiff = dayDiff / 7
-      console.log(weekDiff + '周')
-      var monthDiff = dayDiff / 30
-      console.log(monthDiff + '月')
-      var yearDiff = monthDiff / 12
-      console.log(yearDiff + '年')
-    }
   }
 }
 </script>
@@ -108,64 +81,6 @@ export default {
 .related-video-list {
   display: flex;
   justify-content: space-between;
-}
-
-.related-video-item {
-  width: 280px;
-  /* height: 260px; */
-  background-color: pink;
-}
-
-/* 视频推荐图片和上面的参数 */
-.related-video-item .top-img {
-  cursor: pointer;
-  position: relative;
-  height: 157.5px;
-}
-
-.related-video-item .top-img:hover {
-
-}
-
-.related-video-item .top-img img {
-  /* width: auto;
-  height: auto; */
-  max-width: 100%;
-  max-height: 100%;
-}
-
-.related-video-item .top-img .top-video-label {
-  background-color: #EFCD6D;
-  position: absolute;
-}
-
-.related-video-item .top-img .top-video-time {
-  right: 0;bottom: 0;
-}
-
-.related-video-item .top-img .top-video-audience {
-  left: 0;bottom: 0;
-}
-
-.related-video-item .video-content a{
-  display: block;
-  margin: 0 auto;
-}
-
-.related-video-item .video-content .video-title {
-  height: 38px;
-  overflow: hidden;
-  margin: 10px 0 8px;
-
-  /* 解决文字溢出 */
-  display: -webkit-box;
-  -webkit-line-clamp: 2;
-  -webkit-box-orient: vertical;
-}
-
-.related-video-item .video-content .video-up {
-  font-size: 14px;
-  color: #999;
 }
 
 </style>
--- src/components/video-item.vue
+++ src/components/video-item.vue
@@ -7,8 +7,15 @@
     </div>
     <div class="video-content">
       <a href="#" class="video-title" :title="videoTitle">{{videoTitle}}</a>
-      <a href="#" class="video-up">{{videoUpdater}}</a>
-      <div class="video-uptime">{{videoTimeDiff}}</div>
+      <div class="content-bottom">
+        <div class="content-left">
+          <img src="./marking/marking-img/upimg.jpg" alt="">
+        </div>
+        <div class="content-right">
+          <a href="#" class="video-up">{{videoUpdater}}</a>
+          <div class="video-uptime">{{videoTimeDiff}}</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -21,18 +28,20 @@ export default {
       videoTime: '4:17',
       videoTitle: '[MV]なんでもないや Cover (nandemonaiya) - radwimps Cover by yurisa [君の名はost]',
       videoUpdater: 'yurisa',
-      videoUpdateTime: '2020/2/24 19:29:33',
-      videoViews: '10万',
+      videoUpdateTime: '2019/4/21 10:08:33',
+      videoViews: 2212294,
       videoTimeDiff: ''
     }
   },
   created () {
+    this.simplifyViews()
     this.calculateTimeDiff()
   },
   methods: {
+    simplifyViews: function () {
+      if (this.videoViews > 9999) { this.videoViews = Math.floor(this.videoViews / 10000) + '万' }
+    },
     calculateTimeDiff: function () {
-      console.log('success')
-      const that = this
       const newTime = new Date()
       const updateTime = new Date(this.videoUpdateTime)
       const timeDiff = newTime.getTime() - updateTime.getTime()
@@ -49,13 +58,13 @@ export default {
           const monthDiff = dayDiff / 30
           console.log(monthDiff + '月')
           if (monthDiff >= 12) {
-            that.videoTimeDiff = Math.floor(monthDiff / 12) + '年前'
+            this.videoTimeDiff = Math.floor(monthDiff / 12) + '年前'
             return
           }
-          that.videoTimeDiff = Math.floor(monthDiff) + '个月前'
+          this.videoTimeDiff = Math.floor(monthDiff) + '个月前'
           return
         }
-        that.videoTimeDiff = Math.floor(weekDiff) + '周前'
+        this.videoTimeDiff = Math.floor(weekDiff) + '周前'
       } else if (dayDiff < 1) {
         const hourDiff = dayDiff * 24
         console.log(hourDiff + '时')
@@ -63,15 +72,15 @@ export default {
           const minuteDiff = hourDiff * 60
           console.log(minuteDiff + '分')
           if (minuteDiff < 3) {
-            that.videoTimeDiff = '刚刚'
+            this.videoTimeDiff = '刚刚'
             return
           }
-          that.videoTimeDiff = Math.floor(minuteDiff) + '分钟前'
+          this.videoTimeDiff = Math.floor(minuteDiff) + '分钟前'
           return
         }
-        that.videoTimeDiff = Math.floor(hourDiff) + '小时前'
+        this.videoTimeDiff = Math.floor(hourDiff) + '小时前'
       } else {
-        that.videoTimeDiff = Math.floor(dayDiff) + '天前'
+        this.videoTimeDiff = Math.floor(dayDiff) + '天前'
       }
     }
   }
@@ -87,47 +96,50 @@ export default {
 }
 
 /* 视频推荐图片和上面的参数 */
-.related-video-item .top-img {
+.top-img {
   cursor: pointer;
   position: relative;
   height: 157.5px;
   overflow: hidden;
 }
 
-.related-video-item .top-img img:hover {
+.top-img img:hover {
   transform: scale(1.5);
   transition: .5s;
   animation-direction:alternate;
 }
 
-.related-video-item .top-img img {
+.top-img img, .video-content .content-left img {
   /* width: auto;
   height: auto; */
   max-width: 100%;
   max-height: 100%;
 }
 
-.related-video-item .top-img .top-video-label {
+.top-img .top-video-label {
   /* background-color: #EFCD6D; */
   position: absolute;
+  color: #ffffff;
+  font-size: 12px;
+  text-shadow: 0px 0px 7px rgba(0, 0, 0, .9);
 }
 
-.related-video-item .top-img .top-video-time {
-  right: 0;
-  bottom: 0;
+.top-img .top-video-time {
+  right: 10px;
+  bottom: 5px;
 }
 
-.related-video-item .top-img .top-video-audience {
-  left: 0;
-  bottom: 0;
+.top-img .top-video-audience {
+  left: 10px;
+  bottom: 5px;
 }
 
-.related-video-item .video-content a {
+.video-content a {
   display: block;
   margin: 0 auto;
 }
 
-.related-video-item .video-content .video-title {
+.video-content .video-title {
   height: 40px;
   overflow: hidden;
   margin: 5px 0 8px;
@@ -136,9 +148,26 @@ export default {
   display: -webkit-box;
   -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
+  -moz-box-orient: vertical;
 }
 
-.related-video-item .video-content .video-up,
+.video-content .content-bottom {
+  display: flex;
+}
+
+.video-content .content-bottom .content-left {
+  width: 36px;
+  height: 36px;
+  border-radius: 50%;
+  background-color: aqua;
+  overflow: hidden;
+}
+
+.video-content .content-bottom .content-right {
+  margin-left: 10px;
+}
+
+.video-content .content-bottom .content-right .video-up,
 .video-uptime {
   font-size: 14px;
   color: #999;
