e9c0bd94928023f0b14a947991d346bd16254618
yuan <838003282@qq.com>
2021-02-28 01:04:41 +0800

    顶部栏的改进

--- src/components/topNav.vue
+++ src/components/topNav.vue
@@ -1,19 +1,21 @@
 <template>
-    <el-container class="topNav">
-        <el-container class="navContent">
-            <!-- logo区域 -->
-            <div class="logo">
-                <a href="#"><img src="../assets/Itest.png" alt="logo"></a>
-            </div>
-            <!-- 菜单区域 -->
-            <div class="menuList">
-                <a href="#"><span class="list">产品</span></a>
-                <a href="#"><span class="list">资讯</span></a>
-                <a href="#"><span class="list">关于我们</span></a>
-                <a href="#"><span class="list">帮助反馈</span></a>
-            </div>
-        </el-container>
-    </el-container>
+    <div class="topNav">
+        <div class="wrapper">
+            <el-container class="navContent">
+                <!-- logo区域 -->
+                <div class="logo">
+                    <a href="#"><img src="../assets/Itest.png" alt="logo"></a>
+                </div>
+                <!-- 菜单区域 -->
+                <div class="menuList">
+                    <a href="#"><span class="list">产品</span></a>
+                    <a href="#"><span class="list">资讯</span></a>
+                    <a href="#"><span class="list">关于我们</span></a>
+                    <a href="#"><span class="list">帮助反馈</span></a>
+                </div>
+            </el-container>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -25,39 +27,42 @@ export default {
 </script>
 
 <style scoped>
-    a,a:hover,a:active,a:link,a:focus,a:visited{
-        text-decoration: none;
-    }
-    .topNav{
-        background-color: black;
-        /* height: 100px; */
-        color: rgb(119, 126, 133);
-    }
-    .navContent{
-        /* background-color: blanchedalmond; */
-        /* height: auto; */
-        margin: 0 10vw;
-        display:grid;
-        grid-template-columns: 1fr 3fr;
-    }
-    .logo img {
-        height: 10vh;
-        margin: 15px 0 0 15px;
-    }
-    .logo span{
-        font-size: 18px;
-        position: absolute;
-        top: 35px;
-    }
-    .menuList{
-        /* background-color:skyblue; */
-        display: grid;
-        grid-template-columns: 1fr 1fr 1fr 1fr;
-        place-items: center center;
-        font-size: 18px;
-    }
-    .menuList a span{
-        color: rgb(119, 126, 133);
-        font-weight: 800;
-    }
+a,a:hover,a:active,a:link,a:focus,a:visited {
+    text-decoration: none;
+}
+
+.topNav {
+    background-color: black;
+    color: rgb(119, 126, 133);
+}
+
+.wrapper {
+    width: 1200px;
+    margin: 0 auto;
+}
+
+.navContent {
+    display:grid;
+    grid-template-columns: 1fr 3fr;
+}
+
+.logo img {
+    height: 8vh;
+}
+
+.logo span{
+    font-size: 18px;
+    position: absolute;
+    top: 35px;
+}
+.menuList{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    place-items: center center;
+    font-size: 18px;
+}
+.menuList a span{
+    color: rgb(119, 126, 133);
+    font-weight: 800;
+}
 </style>
